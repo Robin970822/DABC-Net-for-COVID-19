902c0eeff1186b31661ee0c3315d68d8097324e5
RobinHanxy <717740827@qq.com>
2020-11-15 14:15:54 +0800

    Download scripts.

--- download.sh
+++ download.sh
@@ -2,10 +2,17 @@ mkdir -p 2020035365
 cd 2020035365
 gdown --id 1g3Hi_utRCV_c2J6B-QA0sMEM4x8FT_Cx
 unzip 2020035365.zip
-
 cd ..
-mkdir weight
+
+mkdir -p 2020035021
+cd 2020035021
+gdown --id 1u3womSiXn0cmlkMQLikUAcHj28lud4_t
+unzip 2020035021.zip
+cd ..
+
+mkdir -p weight
 cd weight
 gdown --id 1Me-9TqTUMse1lbhGfxj2-CSSipq11nSH
 gdown --id 1_79uQZpZYnik2SbNPm-b4GNnTt96dPOx
 gdown --id 1BhBU6OxfIpMJsYfD5zkHMhhqJXZpmAg5
+cd ..
