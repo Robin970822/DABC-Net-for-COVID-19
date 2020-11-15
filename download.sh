6ec106b7d58fed0d7eb161206f5cdc7f47cefade
RobinHanxy <717740827@qq.com>
2020-11-15 14:11:37 +0800

    Download scripts.

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
-mkdir -p /content/DABC-Net-for-COVID-19/2020035365
-cd /content/DABC-Net-for-COVID-19/2020035365
+mkdir -p 2020035365
+cd 2020035365
 gdown --id 1g3Hi_utRCV_c2J6B-QA0sMEM4x8FT_Cx
 unzip 2020035365.zip
 
-mkdir /content/DABC-Net-for-COVID-19/weight
-cd /content/DABC-Net-for-COVID-19/weight
+cd ..
+mkdir weight
+cd weight
 gdown --id 1Me-9TqTUMse1lbhGfxj2-CSSipq11nSH
 gdown --id 1_79uQZpZYnik2SbNPm-b4GNnTt96dPOx
 gdown --id 1BhBU6OxfIpMJsYfD5zkHMhhqJXZpmAg5
